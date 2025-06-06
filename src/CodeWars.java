bac20b7438631fc741c44f2c0dc82ca26a0d6ef4
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-06 15:10:46 +0300

    codewars

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -1,25 +1,35 @@
 public class CodeWars {
-    public static String getIssuer(String cardNumber) {
-        String type = "";
-        
-        if (cardNumber.length() == 15 && (cardNumber.startsWith("34") || cardNumber.startsWith("37"))) {
-            type = "AMEX";
-        } else if (cardNumber.length() == 16 && (cardNumber.startsWith("6011"))) {
-            type = "Discover";
-        } else if (cardNumber.length() == 16 && (
-                cardNumber.startsWith("51") ||
-                cardNumber.startsWith("52") ||
-                cardNumber.startsWith("53") ||
-                cardNumber.startsWith("54") ||
-                cardNumber.startsWith("55") 
-        )) {
-            type = "Mastercard";
-        } else if ((cardNumber.length() == 16 || cardNumber.length() == 13) && cardNumber.startsWith("4")) {
-            type = "VISA";
-        } else {
-            type = "Unknown";
+    public static String calc(String txt){
+    String firstNumber = txt.substring(0,txt.indexOf(" ")).trim();
+    String lastNumber = txt.substring(txt.lastIndexOf(" "), txt.length()).trim();
+    String signAction = txt.substring(txt.indexOf(" "), txt.lastIndexOf(" ")).trim();
 
+    int countAnswerDot = 0;
+    int countFirstNumber = firstNumber.length();
+    int countLastNumber = lastNumber.length();
+
+    if (countLastNumber == countFirstNumber) {
+        return "";
+    }
+
+        switch (signAction) {
+            case "-":
+                countAnswerDot = countFirstNumber - countLastNumber;
+                break;
+            case "+":
+                countAnswerDot = countFirstNumber + countLastNumber;
+                break;
+            case "*":
+                countAnswerDot = countFirstNumber * countLastNumber;
+                break;
+            case "//":
+                if (countLastNumber == 0) return "";
+                countAnswerDot = countFirstNumber / countLastNumber;
+                break;
+            default:
+                return "";
         }
-        return type;
+
+    return ".".repeat(countAnswerDot);
     }
 }
