fc0f78642cade2f22b97e25312440dc5089c8a65
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-07 10:23:50 +0300

    codewars

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -1,11 +1,23 @@
+import java.util.Arrays;
+
 public class CodeWars {
-    public static String capitalize(String s, int[] ind){
-        StringBuilder sStrBuilder = new StringBuilder(s);
-        for (int num : ind){
-            if (num < s.length()) {
-                sStrBuilder.setCharAt(num, s.toUpperCase().charAt(num));
+    public static String[] dup(String[] arr){
+        String[] res = new String[arr.length];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            char last = 0;
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (last != arr[i].charAt(j)) {
+                    sb.append(last = arr[i].charAt(j));
+                }
             }
+            res[i] = sb.toString();
+            sb.setLength(0);
         }
-        return sStrBuilder.toString();
+        return res;
+        }
+
+        return resArr;
     }
 }
