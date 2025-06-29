20cf237c56038d91d542fbe0c9630924b06f72f0
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-29 22:53:25 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 
 public class Main {
     public static String solution(int value) {
-
-        return "";
+        return "Value is " + String.format("%05d", value);
     }
 
     public static void main(String[] args) {
         String stringAf = "Value is 00005";
-        System.out.println(solution(stringAf));
+        int value = 6;
+        System.out.println(solution(value));
     }
 }
