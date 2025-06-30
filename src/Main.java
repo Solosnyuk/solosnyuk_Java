820119583b5b185043211c6354bd8d29691d171b
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-30 11:33:10 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
-    public static void RedroverSchool (int a, int b) {
-    for (int i = b; i <= a; i += 4) {
-            System.out.println(i);
+    public static void RedroverSchool (String s) {
+        int[] arrayPlus = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrayPlus[i] = array[i] + 15;
         }
+        System.out.println(Arrays.toString(arrayPlus));
     }
 
     public static void main(String[] args) {
         int a = 60;
         int b = 40;
-    RedroverSchool(a, b);
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        String s = "Перестановочный алгоритм быстрого действия";
+
+        RedroverSchool(s);
     }
 }
