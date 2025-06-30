9d5adf28eb57a73b03e4ffbe8c147f10201559bc
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-30 11:17:01 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
-    public static int sumOfElements(int matrix[][]) {
-        int summ = 0;
-        for (int i = 0; i < matrix.length; i ++) {
-            for (int cislo : matrix[i]) {
-                summ += cislo;
-            }
+    public static void RedroverSchool (int a, int b) {
+    for (int i = b; i <= a; i += 4) {
+            System.out.println(i);
         }
-        return summ;
     }
 
     public static void main(String[] args) {
-        String stringAf = "wwwwwwz";
-        int[][] value = {{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(sumOfElements(value));
+        int a = 60;
+        int b = 40;
+    RedroverSchool(a, b);
     }
 }
