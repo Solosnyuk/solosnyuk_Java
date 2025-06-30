6de6b81db58957fe8fbac12b6db0a20bacc609b7
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-06-30 09:26:33 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    public static String alphabetWar(String fight){
-        int countLeft = 0;
-        int countRight = 0;
-
-        for (int i = 0; i < fight.length(); i++) {
-            switch (fight.charAt(i)){
-                case 'w':
-                    countLeft += 4; break;
-                case 'p':
-                    countLeft += 3; break;
-                case 'b':
-                    countLeft += 2; break;
-                case 's':
-                    countLeft += 1; break;
-                case 'm':
-                    countRight += 4; break;
-                case 'q':
-                    countRight += 3; break;
-                case 'd':
-                    countRight += 2; break;
-                case 'z':
-                    countRight += 1; break;
+    public static int sumOfElements(int matrix[][]) {
+        int summ = 0;
+        for (int i = 0; i < matrix.length; i ++) {
+            for (int cislo : matrix[i]) {
+                summ += cislo;
             }
         }
-        if (countLeft > countRight) {
-            return "Let's fight again!";
-        } else if (countLeft < countRight) {
-            return "Right side wins!";
-        }
-        return "Let's fight again!";
+        return summ;
     }
 
     public static void main(String[] args) {
         String stringAf = "wwwwwwz";
-        int value = 6;
-        System.out.println(alphabetWar(stringAf));
+        int[][] value = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(sumOfElements(value));
     }
 }
