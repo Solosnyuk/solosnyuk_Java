a0b85c0784696b3df2f2ac9f0aff49a8039dd8d1
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-07-02 16:14:48 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,24 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void bublSort(int[] sortArr) {
-        for (int i = 0; i < sortArr.length - 1; i++) {
-            for (int j = 0; j < sortArr.length - 1 - i; j++){
-                if (sortArr[j] > sortArr[j + 1]) {
-                    int temp = sortArr[j];
-                    sortArr[j] = sortArr[j + 1];
-                    sortArr[j + 1] = temp;
-                }
-            }
+    public static Map<String, Integer> bublSort(String stroka) {
+        Map<String, Integer> result = new HashMap<>();
+
+        String[] words = stroka
+                .toLowerCase()
+                .split(" ");
+
+        for (String word : words) {
+            result.merge(word, 1, (worldOld1, worldOld2) -> worldOld1 + worldOld2);
         }
-        System.out.println(Arrays.toString(sortArr));
-    }
 
-    public static int[] deleteNth(int[] elements, int maxOccurrences) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        return Arrays.stream(elements)
-                .filter(element -> map.merge(element,1,Integer::sum) <= maxOccurrences)
-                .toArray();
 
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 20, 37, 20, 21 };
-        System.out.println(deleteNth(arr,1));
+        String stroka = "Напишите напишите программу на Java для подсчета количества конкретных слов в строке, используя HashMap";
+        System.out.println(bublSort(stroka));
     }
 }
