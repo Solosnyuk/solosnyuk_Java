53582b102a19cbcb77c5401a5d4ec8b7cf7feb51
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-07-04 16:06:31 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static int substringCount(String fullText, String search) {
-        int count = 0;
-        for (int i = 0; i < fullText.length(); i++) {
-            if (fullText.startsWith(search, i)) {
-                i += search.length() - 1;
-                count++;
-            }
-        }
-        return count;
+    public static String camelCase(String input) {
+        return input.replaceAll("([A-Z])", " $1").trim();
     }
 
     public static void main(String[] args) {
         int num = 53421;
-        String str = "2plus2plus3plus4";
-        System.out.println(substringCount("aa_bbb_cc_dd_b_e","bb"));
+        String str = "camelCasing";
+        System.out.println(camelCase(str));
     }
 }
