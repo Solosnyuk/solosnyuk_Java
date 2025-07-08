592852afaa719df3ff7d2094867cb106081b6ccc
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-07-08 08:35:40 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static String bmi(double weight, double height) {
-       double bm = weight/Math.pow(height,2);
-       if (bm <= 18.5) {
-           return "Underweight";
-       } else if (bm <= 25) {
-           return "Normal";
-       } else if (bm <= 30) {
-           return "Overweight";
-       }  else if (bm > 30) {
-           return "Obese";
-       }
-       return null;
+    public static int countSmileys(List<String> arr) {
+        int count = 0;
+        for (String smile : arr) {
+            if (smile.matches("[:;][-~]?[)D]")) {
+                count++;
+            }
+        }
+        return count;
     }
 
-    public static void Main(String[] args) {
-        String str = "DONAL";
-        System.out.println(bmi(50,1.80));
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add(":)"); a.add("XD"); a.add(":0}"); a.add("x:-"); a.add("):-"); a.add("D:");
+        System.out.println(countSmileys(a));
+
     }
 }
