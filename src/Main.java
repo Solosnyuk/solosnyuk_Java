9466807b7e7b7326e7e275bb89f0c7ea64c6ae38
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-07-29 19:03:18 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,58 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.stream.IntStream.rangeClosed;
+
 public class Main {
-        public static int getCount(String str) {
-           return  (int) str.toLowerCase().chars()
-                    .filter(c -> "aeiou".indexOf(c) != -1)
-                    .count();
+    public static int[] pipeFix(int[] numbers) {
+        return rangeClosed(numbers[0],numbers[numbers.length - 1]).toArray();
+    }
+
+    public static Object[] removeEveryOther(Object[] arr) {
+        return IntStream.range(0, arr.length)
+                .filter(n -> n % 2 == 0)
+                .mapToObj(i->arr[i]).toArray();
+    }
+
+    public static int[] monkeyCount(final int n){
+        return IntStream.range(0,n).toArray();
+
+    }
+
+    public static int[] countPositivesSumNegatives(int[] input){
+
+        if (input.length == 0 || input == null){
+            return new int[]{};
         }
 
-    public static String disemvowel(String str) {
-        return str.replaceAll("[aeiouAEIOU]", "");
+        int posCount = (int) Arrays.stream(input).filter(n-> n > 0).count();
+        int sumNeg = (int) Arrays.stream(input).filter(n -> n < 0).sum();
+        return new int[]{posCount,sumNeg};
     }
 
-    public static int squareDigits(int n) {
-        int[] digits = String.valueOf(n)
-                .chars()
-                .map(Character::getNumericValue)
-                .toArray();
-        return Integer.parseInt(Arrays.stream(digits)
-                .map(v -> v * v)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining()));
+    public static int[] invert(int[] array) {
+        return Arrays.stream(array).map(n -> n * -1).toArray();
+
     }
 
-    public static String highAndLow(String numbers) {
-        int[] nums = Arrays.stream(numbers.split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        int max = Arrays.stream(nums).max().orElseThrow();
-        int min = Arrays.stream(nums).min().orElseThrow();
-        return max + " " + min;
+    public static int rowSumOddNumbers(int n) {
+        return n * n * n;
     }
 
-    public static int sortDesc(final int num) {
-        String[] array = String.valueOf(num).split("");
-        Arrays.sort(array, Collections.reverseOrder());
-        return Integer.valueOf(String.join("", array));
+    public static int squareSum(int[] n) {
+        return Arrays.stream(n).map(t -> t * t).sum();
+    }
+
+    public static int sum(int[] arr){
+        return Arrays.stream(arr).filter(n -> n > 0).sum();
     }
 
         public static void main(String[] args) {
-            System.out.println(sortDesc(15));
+            System.out.println(Arrays.toString(invert(new int[]{1, 2, 3, 4, 5})));
+
         }
     }
 
