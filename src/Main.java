27a661052d20ede608b7054aa590ba2b4b54a73d
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-01 19:43:31 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,78 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static java.util.stream.IntStream.rangeClosed;
-
 public class Main {
-    public static int[] pipeFix(int[] numbers) {
-        return rangeClosed(numbers[0],numbers[numbers.length - 1]).toArray();
-    }
+    public static String whoLikesIt(String... names) {
+        List<String> listName = Arrays.stream(names).collect(Collectors.toList());
 
-    public static Object[] removeEveryOther(Object[] arr) {
-        return IntStream.range(0, arr.length)
-                .filter(n -> n % 2 == 0)
-                .mapToObj(i->arr[i]).toArray();
-    }
-
-    public static int[] monkeyCount(final int n){
-        return IntStream.range(0,n).toArray();
-
-    }
-
-    public static int[] countPositivesSumNegatives(int[] input){
-
-        if (input.length == 0 || input == null){
-            return new int[]{};
+        if (listName.size() == 0){
+            return "no one likes this";
+        }else if (listName.size() == 1){
+            return listName.get(0) +" likes this";
+        } else if (listName.size() == 2) {
+            return listName.get(0) + " and " + listName.get(1) + " likes this";
+        } else if (listName.size() == 3) {
+            return listName.get(0) + ", " + listName.get(1) + " and " + listName.get(2) + " likes this";
+        } else if (listName.size() >= 4) {
+            return listName.get(0) + ", " + listName.get(1) + " and "
+                    + (listName.size() - 2) + " others like this";
         }
-
-        int posCount = (int) Arrays.stream(input).filter(n-> n > 0).count();
-        int sumNeg = (int) Arrays.stream(input).filter(n -> n < 0).sum();
-        return new int[]{posCount,sumNeg};
+        return "";
     }
 
-    public static int[] invert(int[] array) {
-        return Arrays.stream(array).map(n -> n * -1).toArray();
-
+    public static int findIt(int[] a) {
+        HashMap<Integer, Integer> mapIt = new HashMap<>();
+        for (int it : a) {
+            mapIt.merge(it,1,Integer::sum);
+        }
+        for (Map.Entry<Integer,Integer> hash : mapIt.entrySet()) {
+            if (hash.getValue() % 2 != 0) {
+                return hash.getKey();
+            }
+        }
+        return 0;
     }
 
-    public static int rowSumOddNumbers(int n) {
-        return n * n * n;
+    public static  int solution(int number) {
+        int sum = 0;
+        for (int i = 3; i < number; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            } else if (i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 
-    public static int squareSum(int[] n) {
-        return Arrays.stream(n).map(t -> t * t).sum();
-    }
+    public static String[] towerBuilder(int nFloors) {
+        String[] towerBuild = new String[nFloors];
 
-    public static int sum(int[] arr){
-        return Arrays.stream(arr).filter(n -> n > 0).sum();
+        for (int i = 0; i < nFloors; i++) {
+            StringBuilder inter = new StringBuilder();
+            StringBuilder startStr = new StringBuilder();
+
+            for (int j = 0; j < i + 1 ; j++) {
+                inter.append("*");
+
+            }
+            for (int j = 0; j < nFloors - i - 1; j++) {
+                startStr.append(" ");
+            }
+            String nulle = String.valueOf(startStr);
+            String medi = String.valueOf(inter);
+
+            towerBuild[i] = nulle + medi + nulle;
+        }
+        return towerBuild;
     }
 
         public static void main(String[] args) {
-            System.out.println(Arrays.toString(invert(new int[]{1, 2, 3, 4, 5})));
+            System.out.println(Arrays.toString(towerBuilder(6)));
 
         }
     }
