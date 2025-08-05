b8dc9370aedbffb68ab1c090f0269c381f602efc
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-05 15:06:58 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,95 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
-    public static String whoLikesIt(String... names) {
-        List<String> listName = Arrays.stream(names).collect(Collectors.toList());
-
-        if (listName.size() == 0){
-            return "no one likes this";
-        }else if (listName.size() == 1){
-            return listName.get(0) +" likes this";
-        } else if (listName.size() == 2) {
-            return listName.get(0) + " and " + listName.get(1) + " likes this";
-        } else if (listName.size() == 3) {
-            return listName.get(0) + ", " + listName.get(1) + " and " + listName.get(2) + " likes this";
-        } else if (listName.size() >= 4) {
-            return listName.get(0) + ", " + listName.get(1) + " and "
-                    + (listName.size() - 2) + " others like this";
-        }
-        return "";
+    public static double findUniq(double arr[]) {
+        Arrays.sort(arr);
+        if(arr[0]==arr[1])
+            return arr[arr.length-1];
+        else
+            return arr[0];
     }
 
-    public static int findIt(int[] a) {
-        HashMap<Integer, Integer> mapIt = new HashMap<>();
-        for (int it : a) {
-            mapIt.merge(it,1,Integer::sum);
+    public static boolean isNarcissistic(int number) {
+        int summ = 0;
+        int num = number;
+        while (num > 0){
+            int dig = num % 10;
+            num = num / 10;
+            summ += Math.pow(dig, Integer.toString(number).length());
         }
-        for (Map.Entry<Integer,Integer> hash : mapIt.entrySet()) {
-            if (hash.getValue() % 2 != 0) {
-                return hash.getKey();
-            }
+        System.out.println(summ);
+        if (summ == number){
+            return true;
         }
-        return 0;
+        return false;
     }
 
-    public static  int solution(int number) {
-        int sum = 0;
-        for (int i = 3; i < number; i++) {
-            if (i % 3 == 0) {
-                sum += i;
-            } else if (i % 5 == 0) {
-                sum += i;
-            }
+    public static int uniqueValues(int[] arr){
+        Set<Integer> arSet = new HashSet<>();
+        for (int num : arr) {
+            arSet.add(num);
         }
-        return sum;
+        return arSet.size();
     }
 
-    public static String[] towerBuilder(int nFloors) {
-        String[] towerBuild = new String[nFloors];
+    public static int findSameInLine(int[] arr) {
+        if (arr.length == 0) return 0;
 
-        for (int i = 0; i < nFloors; i++) {
-            StringBuilder inter = new StringBuilder();
-            StringBuilder startStr = new StringBuilder();
+        int maxCount = 1;
+        int currentCount = 1;
 
-            for (int j = 0; j < i + 1 ; j++) {
-                inter.append("*");
-
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                currentCount++;
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            } else {
+                currentCount = 1;
             }
-            for (int j = 0; j < nFloors - i - 1; j++) {
-                startStr.append(" ");
-            }
-            String nulle = String.valueOf(startStr);
-            String medi = String.valueOf(inter);
-
-            towerBuild[i] = nulle + medi + nulle;
         }
-        return towerBuild;
+        return maxCount;
     }
 
-        public static void main(String[] args) {
-            System.out.println(Arrays.toString(towerBuilder(6)));
+    public static int [] delElementFromArr(int[]arr, int index){
+        int[] newArr = new int[arr.length - 1];
+        for (int i = 0; i < arr.length; i++) {
+            if (i < index) {
+                newArr[i] = arr[i];
+            } else if (i > index) {
+                newArr[i - 1] = arr[i];
+            }
+        }
+        return newArr;
+    }
 
+    public static void switchMinMax(int [] arr){
+        int indexMin = 0;
+        int indexMax = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                indexMax = i;
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+                indexMin = i;
+            }
+        }
+        arr[indexMin] = max;
+        arr[indexMax] = min;
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        switchMinMax(new int[]{1,2,3,4,5});
         }
     }
 
