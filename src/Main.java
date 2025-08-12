657a863d25bf6efe15073b8426d4cecd9754b45a
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-12 23:10:32 +0300

    codewars

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,62 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class Main {
-    public static double findUniq(double arr[]) {
-        Arrays.sort(arr);
-        if(arr[0]==arr[1])
-            return arr[arr.length-1];
-        else
-            return arr[0];
-    }
-
-    public static boolean isNarcissistic(int number) {
-        int summ = 0;
-        int num = number;
-        while (num > 0){
-            int dig = num % 10;
-            num = num / 10;
-            summ += Math.pow(dig, Integer.toString(number).length());
-        }
-        System.out.println(summ);
-        if (summ == number){
-            return true;
-        }
-        return false;
-    }
-
-    public static int uniqueValues(int[] arr){
-        Set<Integer> arSet = new HashSet<>();
-        for (int num : arr) {
-            arSet.add(num);
-        }
-        return arSet.size();
-    }
-
-    public static int findSameInLine(int[] arr) {
-        if (arr.length == 0) return 0;
-
-        int maxCount = 1;
-        int currentCount = 1;
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                currentCount++;
-                if (currentCount > maxCount) {
-                    maxCount = currentCount;
+    public static void findCountElemAboveAvg(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]){
+                    int sec = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = sec;
                 }
-            } else {
-                currentCount = 1;
             }
         }
-        return maxCount;
     }
 
-    public static int [] delElementFromArr(int[]arr, int index){
-        int[] newArr = new int[arr.length - 1];
+    public static double findSumAvgLines(int[][] arr){
+        double sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (i < index) {
-                newArr[i] = arr[i];
-            } else if (i > index) {
-                newArr[i - 1] = arr[i];
+            for (int dec : arr[i]) {
+                sum += dec;
             }
         }
-        return newArr;
+        return Math.round(sum/ arr.length * 10) / 10.0;
     }
 
-    public static void switchMinMax(int [] arr){
-        int indexMin = 0;
-        int indexMax = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+    public static double findAvgOddElem(int[][] arr){
+        int count = 0;
+        int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                indexMax = i;
-            }
-            if (arr[i] < min) {
-                min = arr[i];
-                indexMin = i;
+            for (int num : arr[i]) {
+                if (num % 2 != 0){
+                    count++;
+                    sum += num;
+                }
             }
         }
-        arr[indexMin] = max;
-        arr[indexMax] = min;
 
-        for (int num : arr) {
-            System.out.print(num + " ");
+        if (count == 0){
+            return 0;
         }
+        return Math.round(sum/count * 10)/ 10.0;
     }
+
+    public static int [] shiftValuesInArr(int[] arr, int shift) {
+        for (int i = 0; i < shift; i++) {
+            int last = arr[arr.length - 1];
+            for (int j = arr.length - 1; j > 0 ; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[0] = last;
+        }
+        return arr;
+    }
+
 
     public static void main(String[] args) {
-        switchMinMax(new int[]{1,2,3,4,5});
-        }
+        System.out.println(Arrays.toString(shiftValuesInArr(new int[]{1,2,3,4,5},3)));
+    }
+
     }
 
