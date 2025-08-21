7bfb08fe5e64444c155ad61cc3fccc9a85bfaa89
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-21 08:43:50 +0300

    codewars arrays

--- src/Main.java
+++ src/Main.java
@@ -1,171 +1,176 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    public static void findCountElemAboveAvg(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] < arr[j + 1]){
-                    int sec = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = sec;
-                }
-            }
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
         }
+        return false;
     }
 
-    public static double findSumAvgLines(int[][] arr){
-        double sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int dec : arr[i]) {
-                sum += dec;
-            }
+    public static int diff21(int n) {
+        if (n <= 21){
+            return 21 - n;
         }
-        return Math.round(sum/ arr.length * 10) / 10.0;
+        return (n - 21) * 2;
     }
 
-    public static double findAvgOddElem(int[][] arr){
+    public static boolean parrotTrouble(boolean talking, int hour) {
+        if (talking){
+            if (hour > 20 || hour < 7){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0, out.length()/2) + word +out.substring( out.length()/2, out.length());
+    }
+
+    public static String extraEnd(String str) {
+        String copy = str.substring(str.length() - 2, str.length());
+        return copy + copy + copy;
+    }
+
+    public static String firstTwo(String str) {
+        if (str.length() >= 2){
+            return str.substring(0, 2);
+        }
+        return str;
+    }
+
+    public static String doubleChar(String str) {
+        String newStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            newStr = newStr + str.charAt(i) + str.charAt(i);
+        }
+        return newStr;
+    }
+
+    public static int countHi(String str) {
+        String search = "hi";
         int count = 0;
-        int sum = 0;
+        for (int i = 0; i < str.length() - search.length() + 1; i++) {
+            if (str.substring(i, i + 2).equals(search)){
+                count++;
+            }
+        }
+        return count;
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int num : arr[i]) {
-                if (num % 2 != 0){
-                    count++;
-                    sum += num;
+    public static boolean catDog(String str) {
+        int countDog = 0;
+        int countCat = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i,i + 3).equals("dog")){
+                countDog++;
+            }
+            if (str.substring(i,i + 3).equals("cat")){
+                countCat++;
+            }
+        }
+        if (countDog == countCat){
+            return true;
+        }
+        return false;
+    }
+
+    public static int countCode(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 3; i++) {
+            if (str.substring(i,i + 2).equals("co") && str.charAt(i + 3) == 'e'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean endOther(String a, String b) {
+        if (a.toLowerCase().startsWith(b.toLowerCase(), a.length() - b.length())  ||
+                b.toLowerCase().startsWith(a.toLowerCase(), b.length() - a.length())){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean xyzThere(String str) {
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("xyz")){
+                if (i > 0 && str.charAt(i - 1) != '.'){
+                    return true;
                 }
+                if (i == 0){
+                    return true;
+                }
+                return false;
             }
         }
-
-        if (count == 0){
-            return 0;
-        }
-        return Math.round(sum/count * 10)/ 10.0;
+        return false;
     }
 
-    public static int [] shiftValuesInArr(int[] arr, int shift) {
-        for (int i = 0; i < shift; i++) {
-            int last = arr[arr.length - 1];
-            for (int j = arr.length - 1; j > 0 ; j--) {
-                arr[j] = arr[j - 1];
+    public static int countEvens(int[] nums) {
+        int count = 0;
+        for(int num : nums){
+            if (num % 2 == 0){
+                count++;
             }
-            arr[0] = last;
         }
-        return arr;
+        return count;
     }
 
-    public static int[] findIndexAndValue(int[] arr) {
+    public static int bigDiff(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums){
+            if (num > max){
+                max = num;
+            }if (num < min){
+                min = num;
+            }
+        }
+        return  max - min;
+    }
+
+    public static int centeredAverage(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         int sum = 0;
-        for (int num : arr){
+        for (int num : nums){
             sum += num;
-        }
-        double srednee = (double) sum / 5;
-        double dif = Double.MAX_VALUE;
-        int[] dep = new int[2];
-
-        for (int i = 0; i < arr.length; i++) {
-            double curentDif = Math.abs(arr[i] - srednee);
-            if (dif > curentDif){
-                dif = curentDif;
-                dep[0] = i;
-                dep[1] = arr[i];
+            if (num > max){
+                max = num;
+            }if (num < min){
+                min = num;
             }
         }
-        return dep;
+        return (sum - min - max)/(nums.length - 2);
     }
 
-    public static void changeSignForElem(int k, int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % k == 0){
-                arr[i] = arr[i] * -1;
+    public static int sum13(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 13){
+                sum += nums[i];
+            }else {
+                i++;
             }
         }
+        return sum;
     }
 
-    public static int[] map(int[] arr) {
-        return Arrays.stream(arr).map(n -> n * 2).toArray();
-    }
 
-    public static String findNeedle(Object[] haystack) {
-        String needle = "needle";
-        for (int i = 0; i < haystack.length; i++) {
-            if (needle.equals(haystack[i])) {
-                return "found the needle at position " + i;
-            }
-        }
-        return "needle not found";
-    }
-
-    public static int[] digitize(long n) {
-        return new StringBuilder()
-                .append(n)
-                .reverse()
-                .chars()
-                .map(Character::getNumericValue)
-                .toArray();
-    }
-
-    public static int findEvenIndex(int[] arr) {
-        int left = 0;
-        int right = Arrays.stream(arr).sum();
-
-        for (int i = 0; i < arr.length; i++) {
-            right -= arr[i];
-            if (left == right){
-                return i;
-            }
-            left += arr[i];
-        }
-        return -1;
-    }
-
-    public static double[] tribonacci(double[] s, int n) {
-        double[] arr = new double[n];
-        if (n < s.length){
-            for (int i = 0; i < n; i++) {
-                arr[i] = s[i];
-            }
-        }else{
-
-        for (int i = 0; i < s.length; i++) {
-            arr[i] = s[i];
-        }
-        }
-
-        for (int i = s.length; i < n; i++) {
-            arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
-        }
-
-        return arr;
-    }
-
-    public static long sumTwoSmallestNumbers(long[] numbers) {
-        return Arrays.stream(numbers)
-                .sorted()
-                .limit(2)
-                .sum();
-    }
-
-    public static int countSheeps(Boolean[] arrayOfSheeps) {
-        return (int) Arrays.stream(arrayOfSheeps)
-                .filter(Objects::nonNull)
-                .filter(Boolean::booleanValue)
-                .count();
-    }
-
-    public static int[] arrayDiff(int[] a, int[] b) {
-        List<Integer> bList = Arrays.stream(b)
-                .boxed()
-                .collect(Collectors.toList());
-        return Arrays.stream(a)
-                .filter(n -> !bList.contains(n))
-                .toArray();
-    }
 
     public static void main(String[] args) {
-        System.out.println(arrayDiff(new int [] {1,2}, new int[] {1}));
-
-    }
-
-    }
+    }    }
 
