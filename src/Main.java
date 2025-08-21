8b6e718c01cbf919806c4f57f9f99de4774c237f
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-21 14:24:42 +0300

    codewars String

--- src/Main.java
+++ src/Main.java
@@ -1,176 +1,75 @@
 import java.util.*;
 
 public class Main {
-    public static boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation) {
-            return true;
-        }
-        return false;
-    }
-
-    public static int diff21(int n) {
-        if (n <= 21){
-            return 21 - n;
-        }
-        return (n - 21) * 2;
-    }
-
-    public static boolean parrotTrouble(boolean talking, int hour) {
-        if (talking){
-            if (hour > 20 || hour < 7){
+    public static  boolean bobThere(String str) {
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == 'b' && str.charAt(i+2) == 'b'){
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean nearHundred(int n) {
-        return ((Math.abs(100 - n) <= 10) ||
-                (Math.abs(200 - n) <= 10));
-    }
-
-    public String helloName(String name) {
-        return "Hello " + name + "!";
-    }
-
-    public static String makeOutWord(String out, String word) {
-        return out.substring(0, out.length()/2) + word +out.substring( out.length()/2, out.length());
-    }
-
-    public static String extraEnd(String str) {
-        String copy = str.substring(str.length() - 2, str.length());
-        return copy + copy + copy;
-    }
-
-    public static String firstTwo(String str) {
-        if (str.length() >= 2){
-            return str.substring(0, 2);
+    public static String mixString(String a, String b) {
+        StringBuilder res=  new StringBuilder();
+        for (int i = 0; i < (Math.max(a.length(), b.length()))-1 ; i++) {
+            if (i <= a.length()-1){
+                res.append(a.charAt(i));
+            }
+            if (i <= b.length()-1){
+                res.append(b.charAt(i));
+            }
         }
-        return str;
+        return res.toString();
     }
 
-    public static String doubleChar(String str) {
-        String newStr = "";
-        for (int i = 0; i < str.length(); i++) {
-            newStr = newStr + str.charAt(i) + str.charAt(i);
-        }
-        return newStr;
-    }
-
-    public static int countHi(String str) {
-        String search = "hi";
+    public static String repeatEnd(String str, int n) {
+        StringBuilder res = new StringBuilder();
         int count = 0;
-        for (int i = 0; i < str.length() - search.length() + 1; i++) {
-            if (str.substring(i, i + 2).equals(search)){
-                count++;
-            }
+        while (count != n){
+            count++;
+            res.append(str.substring(str.length() - n, str.length()));
         }
-        return count;
+        return res.toString();
     }
 
-    public static boolean catDog(String str) {
-        int countDog = 0;
-        int countCat = 0;
-        for (int i = 0; i < str.length() - 2; i++) {
-            if (str.substring(i,i + 3).equals("dog")){
-                countDog++;
-            }
-            if (str.substring(i,i + 3).equals("cat")){
-                countCat++;
+    public static String repeatFront(String str, int n) {
+        StringBuilder res = new StringBuilder();
+        int count = 0;
+        while (count <= n){
+            res.append(str.substring(0, n - count));
+            count++;
+        }
+        return res.toString();
+    }
+
+    public static String repeatSeparator(String word, String sep, int count) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            res.append(word);
+            if (i < count - 1) {
+                res.append(sep);
             }
         }
-        if (countDog == countCat){
+
+        return res.toString();
+    }
+
+    public static boolean prefixAgain(String str, int n) {
+        String pref = str.substring(0,n);
+        if (str.substring(n ).contains(pref)){
             return true;
         }
         return false;
     }
 
-    public static int countCode(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length() - 3; i++) {
-            if (str.substring(i,i + 2).equals("co") && str.charAt(i + 3) == 'e'){
-                count++;
-            }
-        }
-        return count;
+    public static boolean xyzMiddle(String str) {
+        return str.substring().contains("xyz");
     }
-
-    public static boolean endOther(String a, String b) {
-        if (a.toLowerCase().startsWith(b.toLowerCase(), a.length() - b.length())  ||
-                b.toLowerCase().startsWith(a.toLowerCase(), b.length() - a.length())){
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean xyzThere(String str) {
-
-        for (int i = 0; i < str.length() - 2; i++) {
-            if (str.substring(i, i + 3).equals("xyz")){
-                if (i > 0 && str.charAt(i - 1) != '.'){
-                    return true;
-                }
-                if (i == 0){
-                    return true;
-                }
-                return false;
-            }
-        }
-        return false;
-    }
-
-    public static int countEvens(int[] nums) {
-        int count = 0;
-        for(int num : nums){
-            if (num % 2 == 0){
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public static int bigDiff(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int num : nums){
-            if (num > max){
-                max = num;
-            }if (num < min){
-                min = num;
-            }
-        }
-        return  max - min;
-    }
-
-    public static int centeredAverage(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int num : nums){
-            sum += num;
-            if (num > max){
-                max = num;
-            }if (num < min){
-                min = num;
-            }
-        }
-        return (sum - min - max)/(nums.length - 2);
-    }
-
-    public static int sum13(int[] nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 13){
-                sum += nums[i];
-            }else {
-                i++;
-            }
-        }
-        return sum;
-    }
-
-
 
     public static void main(String[] args) {
+        System.out.println(xyzMiddle("AAxyzBB"));
+
     }    }
 
