aeb6dfe3d6b896bd02f0f7a6414765fd4490e8c3
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-28 16:31:30 +0300

    codewars String

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,13 @@ public class Main {
     }
 
     public static int matchUp(int[] nums1, int[] nums2) {
-
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] != nums2[i] && (Math.abs(nums1[i] - nums2[i]) == 2 || Math.abs(nums1[i] - nums2[i]) == 1)){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
