0815d551e0f7b962902b7beb8d5d42dcb2f6a781
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-08-31 19:39:19 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,90 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    private static final Object lock = new Object();
-    private static final int MAX = 10;
-    private static int start = 1;
-    private static boolean turn = true;
+    public static Map<String, String> topping2(Map<String, String> map) {
+        if (map.containsKey("ice cream")){
+            map.put("yogurt",map.get("ice cream"));
+        }
+        if (map.containsKey("spinach")){
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+    public static Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato")){
+            map.put("fries",map.get("potato"));
+        }
+        if (map.containsKey("salad")){
+            map.put("spinach",map.get("salad"));
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB4(Map<String, String> map) {
+
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() != map.get("b").length()) {
+                if (map.get("a").length() > map.get("b").length()) {
+                    map.put("c", map.get("a"));
+                } else {
+                    map.put("c", map.get("b"));
+                }
+                return map;
+            }
+            if (map.get("a").length() == map.get("b").length()) {
+                map.put("a", "");
+                map.put("b", "");
+                return map;
+            }
+        }
+        return map;
+    }
+
+    public static Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string,0);
+        }
+        return map;
+    }
+
+    public Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string,string.length());
+        }
+        return map;
+    }
+
+    public Map<String, String> pairs(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(Character.toString(string.charAt(0)), Character.toString(string.charAt(string.length())));
+        }
+        return map;
+    }
+
+    public static Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string, map.getOrDefault(string, 0) + 1);
+        }
+        return map;
+    }
+
+    public Map<String, String> firstChar(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String string : strings){
+            map.put(Character.toString(string.charAt(0)) ,
+                    map.getOrDefault(Character.toString(string.charAt(0)), "") + string);
+        }
+        return map;
+    }
 
     public static void main(String[] args) {
-
-    Runnable printOne = () -> {
-            while (true){
-                synchronized (lock) {
-                    while (!turn) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    if (start > MAX) {
-                        break;
-                    }
-                    System.out.println(start);
-                    start++;
-
-                    turn = !turn;
-                    lock.notifyAll();
-                }
-            }
-    };
-    Runnable printTwo = () -> {
-            while (true){
-                synchronized (lock) {
-                    while (turn) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    if (start > MAX) {
-                        break;
-                    }
-                    System.out.println(start);
-                    start++;
-
-                    turn = !turn;
-                    lock.notifyAll();
-                }
-            }
-    };
-
-        new Thread(printOne).start();
-        new Thread(printTwo).start();
+        System.out.println(wordCount(new String[]{"a", "b", "a", "c", "b"}));
     }
 }
 
