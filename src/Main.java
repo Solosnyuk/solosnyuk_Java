ff6c68d57c1f6a7f6d2c00ebad70ec30a0c2ab03
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-06 00:12:31 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,104 +1,55 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Main {
-    public boolean scoresIncreasing(int[] scores) {
-        for (int i = 0; i < scores.length - 1; i++) {
-            if (scores[i] > scores[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean scores100(int[] scores) {
-        for (int i = 0; i < scores.length - 1; i++) {
-            if (scores[i] == 100){
-                 if (scores[i] == scores[1 + i]){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean scoresClump(int[] scores) {
-        for (int i = 1; i < scores.length - 1; i++) {
-            if (scores[i + 1] - scores[i] <= 2){
-                if (scores[i] - scores[i - 1] <= 2){
-                    if (scores[i + 1] - scores[i - 1] <= 2){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public int scoresAverage(int[] scores) {
-        return Math.max(average(scores,0,scores.length/2),
-                average(scores,scores.length/2,scores.length));
-    }
-
-    int average(int[] scores, int start, int end) {
-        int[] firstHalf = Arrays.copyOfRange(scores,start,end);
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < firstHalf.length; i++) {
-            sum += firstHalf[i];
-            count ++;
-        }
-        return  sum/count;
-    }
-
-    public int wordsCount(String[] words, int len) {
-        int count = 0;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].length() == len){
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public String[] wordsFront(String[] words, int n) {
-        return Arrays.copyOfRange(words,0, n);
-    }
-
-    public List wordsWithoutList(String[] words, int len) {
-        List<String> strings = new ArrayList<>();
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].length() != len){
-                strings.add(words[i]);
-            }
-        }
-        return strings;
-    }
-
-    public boolean hasOne(int n) {
-        int digit;
-        while (n > 0){
-            digit = n % 10;
-            n = n / 10;
-            if (digit == 1){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public int[] copyEvens(int[] nums, int count) {
+    public int[] copyEndy(int[] nums, int count) {
         int[] ints = new int[count];
-        int counst = 0;
-        for (int i = 0; counst < count; i++) {
-            if (nums[i] % 2 == 0){
-                ints[counst] = nums[i];
-                counst++;
+        int coun = 0;
+
+        for (int i = 0; coun < count; i++) {
+            if (isEndy(nums[i])) {
+                ints[coun] = nums[i];
+                coun++;
             }
         }
+
         return ints;
     }
+
+    boolean isEndy(int n){
+        if (n >= 0 && n <=10 || n >= 90 && n <= 100){
+            return true;
+        }
+        return false;
+    }
+
+    public String[] wordsWithout(String[] words, String target) {
+        StringBuilder strings = new StringBuilder();
+        for (String word : words) {
+            if (!word.equals(target)){
+                strings.append(word);
+                strings.append(" ");
+            }
+        }
+        return strings.toString().split(" ");
+    }
+
+    public int sumHeights(int[] heights, int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end - 1 ; i++) {
+            sum += Math.abs(heights[i] - heights[i + 1]);
+        }
+        return sum;
+    }
+
+    public int sumHeights2(int[] heights, int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end - 1 ; i++) {
+            int plus = (heights[i] - heights[i + 1]);
+            sum += (plus < 0) ? Math.abs(plus * 2) : plus;
+        }
+        return sum;
+    }
+
 
     public static void main(String[] args) {
 
