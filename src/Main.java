a17df70ce08282499fc59ee176e4af0feb1a110f
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-11 08:57:27 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,69 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
-    public static int maxBlock(String str) {
-        if (str.isEmpty()) return 0;
+    public int bunnyEars(int bunnies) {
+        if (bunnies == 0) return 0;
+        return 2 + bunnyEars(bunnies - 1);
+    }
 
-        int max = 0;
-        int count = 0;
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            map.put(strings[i], 0);
+        }
+        return map;
+    }
 
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(i - 1)){
-                count++;
-                if (count > max){
-                    max = count;
-                }
-            } else {
-                count = 0;
+    public Map<String, Integer> wordLen(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string,string.length());
+        }
+        return map;
+    }
+
+    public Map<String, String> pairs(String[] strings) {
+        Map<String,String> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(Character.toString(string.charAt(0)),
+                    Character.toString(string.charAt(string.length())));
+        }
+        return map;
+    }
+
+    public Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings) {
+            map.put(s, map.getOrDefault(s,0) + 1);
+        }
+        return map;
+    }
+
+    public Map<String, String> firstChar(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(Character.toString(string.charAt(0)),
+                   map.getOrDefault(Character.toString(string.charAt(0)), "") + string);
+        }
+        return map;
+    }
+
+    public String wordAppend(String[] strings) {
+        String result = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string,map.getOrDefault(string,0) + 1);
+
+            if ((map.getOrDefault(string, 0) + 1) % 2 == 0) {
+                result += (string);
             }
         }
-        return max + 1;
-    }
-
-    public static String sameEnds(String string) {
-        String sub = "";
-        for (int i = 0; i <= string.length() / 2 ; i++) {
-            if (string.substring(0, i).equals(string.substring(string.length() - i))){
-                sub = string.substring(0, i);
-            }
-        }
-        return sub;
-    }
-
-    public static boolean evenlySpaced(int a, int b, int c) {
-        int max = (Math.max(a, c) < b) ? b : Math.max(a,c);
-        int min = (Math.min(a, c) < b) ?  Math.min(a,c) : b;
-        int sred = (a + b + c) - (max + min);
-        if ((max - sred) == (sred - min)){
-            return true;
-        }
-        return false;
-    }
-
-    public int maxSpan(int[] nums) {
-        return nums.length - 1;
-
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(evenlySpaced(9, 10, 11));
     }
 }
 
