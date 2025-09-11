306d77a13ad1218ac8d09f098b96ff5700eb67ca
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-11 17:05:08 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public int bunnyEars(int bunnies) {
@@ -61,6 +59,62 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public int maxSpan(int[] nums) {
+        int span = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (!map.containsKey(num)){
+                map.put(num,i);
+            }
+            int sp = i - map.get(num) + 1;
+            span = Math.max(span, sp);
+        }
+        return span;
+    }
+
+    public int[] fix34(int[] nums) {
+        List<Integer> indexList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 4 && (nums[i - 1] != 3 || nums[i] == 0)){
+                indexList.add(i);
+            }
+        }
+
+        int indexFour = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 3 && nums[i + 1] != 4){
+                int dep = nums[i + 1];
+                int swap = indexList.get(indexFour++);
+                nums[swap] = dep;
+                nums[i + 1] = 4;
+            }
+        }
+        return nums;
+    }
+
+    public int[] fix45(int[] nums) {
+        List<Integer> indexList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 5){
+                indexList.add(i);
+            }
+        }
+
+        int indexFive = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 4 && nums[i + 1] != 5){
+                int dep = nums[i + 1];
+                int swap = indexList.get(indexFive++);
+                nums[swap] = dep;
+                nums[i + 1] = 5;
+            }
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
