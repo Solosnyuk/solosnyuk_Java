4e24307dddbef021a2138937baf8be2e1f7de302
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-17 22:33:36 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,42 @@
-import java.math.BigInteger;
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Main {
-    //Напишите код, который профильтрует дублирующиеся элементы
-    // в массиве и выведет их в виде списка.
-    public static List<Integer> duplicate(int[] arr){
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : arr) {
-            map.put(num, map.getOrDefault(num,0) + 1);
+    public static int[] seriesUp(int n) {
+        int[] intArr = new int[n*(n + 1)/2];
+        int index = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                intArr[index++] = j;
+            }
         }
-        return map.entrySet().stream()
-                .filter(n -> n.getValue() > 1)
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
+        return intArr;
     }
 
-    //Дайте пример кода, который сортирует
-    // список строк с использованием коллекции.
+    public static int maxMirror(int[] nums) {
+        int maxCount = 0;
+        int count = 0;
+        int indexLeft = 0;
+        int indexRight = 0;
 
-    public static void showList(String[] array) {
-       Arrays.sort(array);
-    }
-
-    //Напишите функцию, которая переворачивает число
-    // (порядок цифр в нем изменяет на противоположный, инвертирует).
-    public static long doInvert(long number) {
-        long result = 0;
-        while (number > 0){
-            long dig = number % 10;
-            result = result * 10 + dig;
-            number = number / 10;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - j; j++) {
+                if (nums[i + indexLeft++] == nums[j - indexRight++]){
+                    count++;
+                    if (count > maxCount){
+                        maxCount = count;
+                    }
+                }else{
+                    indexLeft = 0;
+                    indexRight = 0;
+                }
+            }
         }
-
-        return result;
-    }
-
-    // Напишите метод проверки, является ли введенное число простым числом
-    public static boolean checkPrime(int n) {
-        return BigInteger.valueOf(n).isProbablePrime(10);
-    }
-
-    //Напишите метод поиска двух самых больших значений в массиве.
-    public static int[] getTwoMaxValues(int[] nums) {
-        return Arrays.stream(nums)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .limit(2)
-                .mapToInt(Integer::intValue)
-                .toArray();
-    }
-
-    // Напишите код, который удаляет из строки лишние пробелы.
-    public  static String deletS(String input) {
-        return input.replaceAll(" +", " ");
-    }
-
-    // Напишите код, проверяющий, является ли введенное число палиндромом
-    public static Boolean chekPalindrom(int num){
-
-        return true;
+        return maxCount;
     }
 
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(maxMirror(new int[]{1, 2, 3, 8, 9, 3, 2, 1}));
     }
 }
 
