5b025a0bba90a67add5a9875f086c9c82500ca8d
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-21 22:21:18 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,35 @@
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class Main {
-    public static String solve(final String str) {
-        int countLow = 0;
-        int countUp = 0;
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
 
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isUpperCase(str.charAt(i))){
-                countUp++;
-            }else{
-                countLow++;
-            }
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public String makeOutWord(String out, String word) {
+        return out.substring(0, out.length()/2) + word + out.substring(out.length()/2, out.length());
+    }
+
+    public String extraEnd(String str) {
+        return str.substring(str.length()-2,str.length())
+                + str.substring(str.length()-2,str.length()) + str.substring(str.length()-2,str.length());
+    }
+
+    public String firstTwo(String str) {
+        if (str.length() < 2){
+            return str;
         }
-        return (countLow >= countUp)? str.toLowerCase() : str.toUpperCase();
+        return str.substring(0,2);
     }
-
-    public static int[] minMax(int[] arr) {
-        int[] arrMinMax = new int[2];
-        arrMinMax[0] = Arrays.stream(arr).min().getAsInt();
-        arrMinMax[1] = Arrays.stream(arr).max().orElse(1);
-        return arrMinMax;
-    }
-
-    public String[] mergeTwo(String[] a, String[] b, int n) {
-        return Stream.concat(Arrays.stream(a),Arrays.stream(b))
-                .distinct()
-                .sorted()
-                .limit(3)
-                .toArray(String[]::new);
-    }
-
-    public boolean makeBricks(int small, int big, int goal) {
-        int maxBigLength = Math.min(big, goal / 5) * 5;
-
-        // Оставшуюся длину должны покрыть маленькие кирпичи
-        int remaining = goal - maxBigLength;
-
-        // Проверяем, хватает ли маленьких кирпичей
-        return remaining <= small;
-    }
-
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(minMax(new int[]{1,2,3,4})));
+
+
     }
 }
 
