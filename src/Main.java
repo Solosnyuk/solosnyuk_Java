0172825a5cca5e4751b6f388d7b19f962ab3240e
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-24 15:39:07 +0300

    cc

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,37 @@ public class Main {
         return result;
     }
 
+    public int sum2(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        if (nums.length <= 2){
+            return Arrays.stream(nums).sum();
+        }
+        return nums[0] + nums[1];
+    }
+
+    public int[] middleWay(int[] a, int[] b) {
+        int[] arr = new int[]{2};
+        arr[0] = Arrays.stream(a).sum() / a.length;
+        arr[1] = Arrays.stream(a).sum() / b.length;
+        return arr;
+    }
+
+    public int[] makeEnds(int[] nums) {
+        return new int[]{nums[0],nums[nums.length - 1]};
+    }
+
+    public boolean has23(int[] nums) {
+        for (int num : nums) {
+            if (num == 2 || num == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(rotateLeft3(new int[]{1,2,3,4})));
     }
