1842ef03a8c9b0d6e729b33461c567ab5be09416
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-09-28 10:55:09 +0300

    cc

---
+++ src/three/Obstacle/Obstacle.java
@@ -0,0 +1,9 @@
+package three.Obstacle;
+
+public class Obstacle {
+    Integer length;
+
+    public Integer getLength(){
+        return length;
+    }
+}
---
+++ src/three/Obstacle/Poole.java
@@ -0,0 +1,13 @@
+package three.Obstacle;
+
+public class Poole extends Obstacle {
+    Integer  length;
+
+    public Poole(Integer length){
+        this.length = length;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+}
