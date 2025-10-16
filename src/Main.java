43c580951a6b631895d933657a3a411956543f20
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-10-16 20:14:18 +0300

    цв

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -33,5 +34,46 @@ public class Main {
         List<String> list = Arrays.asList(arr);
         Collections.reverse(list);
         return String.join(" ", list);
+    }
+
+    public static BigInteger factorial(int n) {
+        return factorial(n - 1);
+    }
+
+    public static Map<String, Integer> searchDuplicate(List<String> list) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : list) {
+            map.put(string, map.getOrDefault(string, 0) + 1);
+        }
+        return map;
+    }
+
+    public static List<String> removeDuplicate(List<String> list) {
+        return list.stream().distinct().collect(Collectors.toList());
+    }
+
+    public static boolean anagram(String string, String silent) {
+        if (string.length() != silent.length()) {
+            return false;
+        }
+
+        char[] chars = string.toCharArray();
+        char[] chars2 = silent.toCharArray();
+
+        return Arrays.equals(chars,chars2);
+    }
+
+    public static String searchMaxLengthWord(String string) {
+        String[] strings = string.split(" ");
+
+        return Arrays.stream(strings)
+                .max(Comparator.comparingInt(String::length))
+                .orElse("");
+    }
+
+    public static List<String> sortList(List<String> list) {
+        return list.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
     }
 }
