0f4a7ea948dbb0d8b23a1db53eef922c1a095c72
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-10-24 13:46:20 +0300

    цв

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,38 @@
-import java.math.BigInteger;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
     }
 
-    public static String reversString(String string) {
-        return new StringBuilder(string).reverse().toString();
-    }
-
-    public static Boolean palindromChek(String string) {
-        String reversStr = new StringBuilder(string).reverse().toString();
-        return string.equals(reversStr);
-    }
-
-    public static BigInteger factorial(int n) {
-        return BigInteger.valueOf(n).multiply(factorial(n - 1));
-    }
-
-    public static Integer searchTwoMax(int[] arr) {
+    public static int[] filterConvert(String[] arr) {
         return Arrays.stream(arr)
-                .max()
-                .getAsInt();
+                .filter(word -> word.matches("\\d+"))
+                .mapToInt(Integer::parseInt)
+                .filter(n -> n % 2 == 0)
+                .toArray();
     }
 
-    public static List<String> removeDuplicate(List<String> list) {
-        return list.stream().distinct().collect(Collectors.toList());
-    }
+    public static void sortEmploye(List<Employe> list) {
+        Map<String, DoubleSummaryStatistics> depart = list.stream()
+                .collect(Collectors.groupingBy(Employe::getDepartament,
+                        Collectors.summarizingDouble(Employe::getSalary)));
 
-    public static void fizzBuzz(int n) {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            }
+        for (Map.Entry<String, DoubleSummaryStatistics> stringDoubleSummaryStatisticsEntry : depart.entrySet()) {
+            System.out.println(stringDoubleSummaryStatisticsEntry);
         }
     }
 
-    public static boolean anagramCheck(String one, String two) {
-        char[] chars = one.toCharArray();
-        char[] chars2 = two.toCharArray();
+    public static LinkedHashSet<Integer> sortFilter(List<Integer> list) {
+        Map<Integer, Long> frequencyMap = list.stream()
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
 
-        Arrays.sort(chars);
-        Arrays.sort(chars2);
-
-        return Arrays.equals(chars, chars2);
-    }
-
-    public static Map<Character, Integer> sortChar(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0) +1);
-        }
-
-        return map;
+        return list.stream()
+                .filter(n -> frequencyMap.get(n) > 1)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
 
-    
 }
