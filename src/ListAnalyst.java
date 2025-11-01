ae8c9e7074f588dff29ee338f039be33deb85c35
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-11-01 08:38:00 +0300

    ыв

---
+++ src/ListAnalyst.java
@@ -0,0 +1,33 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListAnalyst {
+    public static Map<String, Long> groupPassedList(List<TestCase> list) {
+        return list.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                testCase -> testCase.getStatus,
+                                Collectors.counting()
+                        )
+                );
+    }
+
+    public static List<String> failedIdList(List<TestCase> list) {
+        return list.stream()
+                .filter(
+                        testCase -> "FAILED".equals(testCase.getStatus))
+                .sorted(
+                        Comparator.comparingLong(TestCase::getDurationMs)
+                                .reversed()
+                ).collect(Collectors.toList());
+    }
+
+    String str = String.format(
+            "efgdhgfkjsdhfjkbsdkfbksjf odgjjsdkhjj   sdjghjksghjh %s sfdfj %d klgjjfjlfg %b",
+            "Hello",
+            10,
+            true
+    );
+}
