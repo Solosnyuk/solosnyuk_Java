c06c08c83f887dd6be7af66cfcc4fb5eba7e413f
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-11-03 16:26:15 +0300

    ыв

--- src/Main.java
+++ src/Main.java
@@ -5,120 +5,52 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("ii", "i", "iiiiiiiiiiiii");
-        System.out.println(sortGlasnii(list));
+
 
     }
 
-    public static String concatList(Collection<String> list) {
+    public static Map<String, List<String>> filtrAndGroup(List<User> list) {
         return list.stream()
-                .collect(Collectors.joining(" "));
+                .filter(user -> user.getAge() > 30)
+                .collect(Collectors.groupingBy(User::getCity));
     }
 
-    public static String reversString(String string) {
-        return new StringBuilder(string).reverse().toString();
-    }
-
-    public static Integer uniqChar(String string) {
-        Set<Character> uniqueChars = new HashSet<>();
-
-        for (char c : string.toCharArray()) {
-            uniqueChars.add(c);
-        }
-        return uniqueChars.size();
-    }
-
-    public static long sumQadrNum(Collection<Integer> collection) {
-        return collection.stream()
-                .mapToLong(n -> (long) n * n)
-                .sum();
-    }
-
-    public static String firstBigBeat(List<String> list) {
+    public static Order maxElement(List<Order> list) {
         return list.stream()
-                .filter(word -> Character.isUpperCase(word.charAt(0)))
-                .findFirst()
-                .orElse("Нету строк");
+                .filter(order -> "PAID".equals(order.getStatus()))
+                .max(Comparator.comparingInt(Objects::getAmount))
+                .orElse(null);
     }
 
-    public static String delTrim(String string) {
-        return string.trim();
+    public static String strString(List<String> list) {
+        String s = list.stream()
+                .collect(Collectors.joining("; "))
+                .toString();
+
+        return "[" + s + "]";
     }
 
-    public static List<String> alphabeatSort(List<String> list) {
+    public static List<String> uniqList(List<String> list) {
         return list.stream()
-                .sorted(String::compareToIgnoreCase)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static String delGlas(String string) {
-        return string.replaceAll("[aeiuo]", "");
-    }
-
-    public static List<String> preobNumber(List<Integer> list) {
+    public static List<Employee> sortEmployeeList(List<Employee> list) {
         return list.stream()
-                .map(num -> "#" + num)
+                .sorted(Comparator.comparing(Employee::getDepartment)
+                        .thenComparing(Comparator.comparingInt(Employee::getSalary).reversed()))
                 .toList();
     }
 
-    public static Map<String, Long> findM(List<String> list) {
+    public static Map<String, Integer> countMap(List<String> list) {
         return list.stream()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-    }
-
-    public static List<Integer> bolleeOdnogo(List<Integer> list) {
-        Map<Integer, Long> map = list.stream()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-        List<Integer> list1 = new ArrayList<>();
-
-        for (Map.Entry<Integer, Long> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                list1.add(entry.getKey());
-            }
-        }
-        return list;
-    }
-
-    public static List<String> sortGlasnii(List<String> list) {
-        Map<String, Long> map = new LinkedHashMap<>();
-
-        for (String string : list) {
-            Long raz = Arrays.asList(string.toLowerCase().toCharArray()).stream()
-                    .filter(w -> w.equals("[aiuoe]"))
-                    .count();
-            map.put(string, raz);
-        }
-        return map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .toList();
-    }
-
-    public static List<String> sortEmployee(List<Employee> list) {
-        return list.stream()
-                .filter(employee -> employee.getAge > 30 && "Developer".equals(employee.getStatus))
-                .map(Employe::getName)
-                .collect(Collectors.toList());
-    }
-
-    public static Integer countUniq(String string) {
-        return Arrays.stream(string.split(" "))
-                .map(word -> word.toLowerCase().replaceAll("[,./')(]", ""))
-                .map(word -> !word.isEmpty())
-                .collect(Collectors.toSet())
-                .size();
-    }
-
-    public static Map<Status, List<Order>> mapOrder(List<Order> list) {
-        return list.stream()
-                .collect(Collectors.groupingBy(Order::getStatus));
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
     }
 }
