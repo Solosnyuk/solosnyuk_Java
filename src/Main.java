5db4222fd455a6a4e0158e79c01526aa91d7e9fa
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-11-04 16:05:07 +0300

    //

--- src/Main.java
+++ src/Main.java
@@ -10,45 +10,55 @@ import static java.util.stream.Collectors.toList;
 public class Main {
     public static void main(String[] args) {
 
-
     }
 
-    public static Map<String, List<String>> filtrAndGroup(List<User> list) {
-        return list.stream()
-                .filter(user -> user.getAge() > 30)
-                .collect(Collectors.groupingBy(User::getCity));
+    public static Map<String, List<String>> filterGroupMap(List<String> list) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : list) {
+            String city = s.substring(0,s.indexOf(':'));
+            String name = s.substring(s.indexOf(':') + 1);
+
+            List<String> names = map.getOrDefault(city, new ArrayList<>());
+            names.add(name);
+            map.put(city, names);
+        }
+        return map;
     }
 
-    public static Order maxElement(List<Order> list) {
+    public static Integer maxEvenInt(List<Integer> list) {
         return list.stream()
-                .filter(order -> "PAID".equals(order.getStatus()))
-                .max(Comparator.comparingInt(Objects::getAmount))
+                .filter(num -> num % 2 == 0)
+                .max(Comparator.naturalOrder())
                 .orElse(null);
     }
 
-    public static String strString(List<String> list) {
-        String s = list.stream()
-                .collect(Collectors.joining("; "))
-                .toString();
-
-        return "[" + s + "]";
+    public static String conString(List<String> list) {
+        return "["+ String.join("; ", list) + "]";
     }
 
-    public static List<String> uniqList(List<String> list) {
+    public static List<String> sortLegthList(List<String> list) {
         return list.stream()
                 .distinct()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static List<Employee> sortEmployeeList(List<Employee> list) {
+    public static List<String> sortAlphabeatList(List<String> list) {
         return list.stream()
-                .sorted(Comparator.comparing(Employee::getDepartment)
-                        .thenComparing(Comparator.comparingInt(Employee::getSalary).reversed()))
+                .sorted(Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.naturalOrder()))
                 .toList();
     }
 
-    public static Map<String, Integer> countMap(List<String> list) {
+    public static Integer sumEvInteger(List<Integer> list) {
+        return list.stream()
+                .filter(num -> num % 3 == 0 && num % 5 != 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static Map<String, Integer> countWordMap(List<String> list) {
         return list.stream()
                 .collect(Collectors.groupingBy(Function.identity(),
                         Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
