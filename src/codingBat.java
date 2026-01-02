993b42b22d7e96acd341e700c1969bc5cfa84f0b
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2026-01-02 11:17:12 +0300

    codingBat

--- src/codingBat.java
+++ src/codingBat.java
@@ -98,4 +98,83 @@ public class codingBat {
         return str.substring(0,3) + str.substring(0,3) + str.substring(0,3);
     }
 
+    public String backAround(String str) {
+        return str.substring(str.length() - 1) + str + str.substring(str.length() - 1);
+    }
+
+    public boolean or35(int n) {
+            if (n % 3 == 0 || n % 5 == 0){
+                return true;
+            }
+            return false;
+    }
+
+    public String front22(String str) {
+        if (str.length() <= 2) {
+            return str + str + str;
+        }
+        return str.substring(0,2) + str + str.substring(0,2);
+    }
+
+    public boolean startHi(String str) {
+        if (str.startsWith("hi")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        if (temp1 < 0 && temp2 > 100) {
+            return true;
+        } else if (temp1 > 100 && temp2 < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean in1020(int a, int b) {
+        if (a <= 20 && a >= 10) {
+            return true;
+        } else if (b <= 20 && b >= 10) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasTeen(int a, int b, int c) {
+        if (a >= 13 && a <= 19) {
+            return true;
+        } else if (b >= 13 && b <= 19) {
+            return true;
+        } else if (c >= 13 && c <= 19) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+    public String delDel(String str) {
+        if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+        return str;
+    }
+
+    public boolean mixStart(String str) {
+        if (str.startsWith("ix",1)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String startOz(String str) {
+
+    }
+
 }
